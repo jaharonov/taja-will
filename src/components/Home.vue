e64600c1240e3b5030d178ab96db529a9b8786a7
Jaffa Aharonov <jaharonov@gmail.com>
2018-12-13 13:17:43 -0600

    link issues

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -137,7 +137,50 @@
   </div>
 </template>
 
+<style>
+a:link {
+  text-decoration: none;
+}
+h1, h2 {
+  font-weight: normal;
+}
 
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+/* mouse over link */
+a:hover {
+  color: #3aa173;
+}
+
+/* selected link */
+a:active {
+  color: lightcoral;
+}
+
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  position: relative;
+}
+</style>
 <script>
   import Parallax from 'vue-parallaxy'
   export default {
