0540e7ddfdd1250d0692ddd98d0e0fcdf464b066
Jaffa Aharonov <jaharonov@gmail.com>
2018-12-12 21:31:43 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+
+  </head>
+  <b-container class="p-5">
+  <div id="app" variant="transparent">
+    <div class="fixed-top p-2">
+    <header>
+      <h1 class="text-left">TAJA WILL</h1>
+    </header>
+<div class="row">
+<div class="col-3">
+
+   </div>
+   <div class="col-3">
+         
+   </div>
+   </div>
+ </div>  
+ <div class="row p-5"></div>
+   
+<!-- navbar-1.vue -->
+    <router-view></router-view>
+    
+      <!-- <div class="row fixed-bottom"> -->
+      
+   
+    <footer class="pb-2">
+      <br/>
+      <b-container class="pb-2">
+      <a href="https://www.facebook.com/motorcity.tapfest" target="_blank"><b-img :src="'./static/facebook.png'"/></a>
+      <a href="https://www.instagram.com/motorcitytapfest/" target="_blank"><b-img :src="'./static/instagram.png'"/></a>
+      <a href="https://twitter.com/motorcitytap" target="_blank"><b-img :src="'./static/twitter.png'"/></a>
+      <br/>
+      </b-container>
+       <span>&copy;2018 Website by <a href="http://www.jaharonov.com" target="_blank">J Aharonov</a>.</span>
+    </footer>
+    <!-- </div> -->
+    
+    
+  
+
+  
+</div>
+</b-container>
+  </html>
+</template>
+
+
+<script type="javascript">
+export default {
+  name: 'app',
+  images: [
+        { src: './static/twitter.png', alt: 'Twitter logo' },
+        { src: './static/instagram.png', alt: 'Instagram logo' },
+        { src: './static/facebook.png', alt: 'Facebook logo' }
+  ],
+  methods: {
+    reload: function() {    
+       console.log('what is it loading?')
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,151 @@
+<template>
+
+  <div class="hello">
+  <b-row>
+  <b-col class="p-3">
+    <h4 class="text-left">Socially Engaged Artist & Healing Practitioner</h4>
+    <p class="text-justify">
+      My work animates questions of identity. I value seeing the body in a spectrum from virtuosic spontaneity to blunt gestural unison. 
+      My aesthetics insist on attention to revolving state-based material for example a sense of loneliness, or control, or tenderness, 
+      or humor, or dissonance, or ritual, or a combination; resulting in multi-dimensional layering of physical and vocal expressivity. 
+      Themes in my work comprise of collective humanity; how we exist in our mundane and extraordinary socio-cultural environments. 
+      Identity is at the core of my values and how it unfolds defines my creative process. In my recently published article for Contact 
+      Quarterly, I write: “Folks with marginalized identities are somatically and psychically guarded, from generations past and a lifetime 
+      of systemic oppression; it is in the body and it will enter the dance.” 
+    </p>
+    <p class="text-justify">
+      My work centers relationships. I crave an aesthetic of closeness; which allows performers to move with intimacy and risk, with each other 
+      and in relationship to an audience. When we come into a moment of connection, we do it with the intention to completely engage each other, 
+      without representing intimacy, we commit to being intimate. This is part of the magic, using intention, improvisation and design to create 
+      new works that comment on how I perceive contemporary humanity.
+    </p>
+
+  </b-col>
+  <b-col lg="7" class="p-3">
+  <parallax :speed-factor="0.6" :parallax="true">
+  <b-img center src="/static/bestever.jpg" fluid/>
+  </parallax>
+  </b-col>
+  <b-col lg="6" class="p-3">
+  <parallax :speed-factor="1" :parallax="true">
+  <b-img center src="/static/headshot4.jpg" fluid/>
+  </parallax>
+  </b-col>
+  <b-col class="p-3 text-left">
+    <h4>Choreographer & Performer</h4>
+    <p class="text-left">
+      <strong>Gospels of Oblivion: To the End 2018</strong>
+    </p>
+    <p class="text-justify">
+      ‘Uniting sequins and survivalism for an absorbing exploration into the waning days of humanity...the situation may seem dire, but Will envisions a future where a touch of glamour becomes a coping mechanism for doom. — Dance Critic Caroline Palmer comments on <a href="http://www.startribune.com/minnesota-choreographers-serve-up-dance-for-times-of-duress-at-candy-box-festival/481948751/" target="_blank">Gospels of Oblivion: To the End</a>,  May 2018 
+
+Presented by ARENA Dances and the Candy Box Dance Festival at the Southern Theater, with additional support from the Minnesota State Arts Board Artist Initiative Award 2018, and Metropolitan Regional Arts Council Next Step Award 2016.
+
+    </p>
+    <p class="text-justify">
+     Presented by ARENA Dances and the Candy Box Dance Festival at the Southern Theater, with additional support from the Minnesota State Arts Board Artist Initiative Award 2018, and Metropolitan Regional Arts Council Next Step Award 2016.
+    </p>
+    <b-img class="p-4" center src="/static/dn3_2387.jpg" fluid/>
+    
+    <!-- <parallax :speed-factor="1">
+  <b-img center src="/static/ciwbrian.jpg"/>
+  </parallax> -->
+  </b-col>
+  <b-row>
+    <h2><strong><i>'She's after connection. Looking at Will is like opening the door to a fire: she glows'</i></strong> Lightsey Darst for MN Artists</h2>
+  </b-row>
+  <b-col lg="6" class="p-3">
+    <h4 class="text-left">Biography</h4>
+    <p class="text-justify">
+      Taja Will is a queer, Chilean adoptee, performer, choreographer, therapist and restorative justice facilitator. Her approach integrates improvisation, somatic modalities, text and vocals in contemporary  performance. Will’s aesthetic is one of spontaneity, bold choice making, sonic and kinetic partnership and the ability to move in relationship to risk and intimacy.  She explores visceral connections to current socio-cultural realities through feminine archetypes and everyday magic.
+    </p>
+    <p class="text-justify">
+      Will’s work has been presented throughout the Twin Cities and across the United States. Including local performances at the Walker Art Center Choreographer’s Evening, the Red Eye Theater’s New Works 4 Weeks, the Radical Recess series, Right Here Showcase and the Candy Box Dance Festival. Will received a Right Here Showcase commission, Jerome Travel Study Award and Foundation for Contemporary Arts Emergency Grant for her recent solo work Bruja // Fugitive Majesty. Most recently her futuristic trio Gospels of Oblivion: To the End received support from the MRAC Next Step Award to tour to the Lion’s Jaw Festival in Boston and MSAB Artist Initiative grant for it’s Twin Cities premiere presented by the Candy Box Dance Festival in May 2018. Will is the recipient of a 2018 McKnight Choreography Fellowship, administered by the Cowles Center and funded by The McKnight Foundation.
+    </p>
+    <p class="text-justify">
+      In addition to her own work Will has collaborated and danced with Sara Shelton Mann, Rosy Simas, Aniccha Arts (Pramila Vasudevan), Deborah Jinza Thayer, Blake Nellis, Tim Rehborg, Off Leash Area, Vanessa Voskuil, Body Cartography Project and Miguel Gutierrez among others. Will maintains a private healing practice blending modalities of healing justice work with developmental psychotherapy and somatic bodywork.
+    </p>
+    <p class="text-justify">
+      An an educator Will has been a guest teacher and adjunct faculty at several institutions locally and throughout the United States. She has presented workshops at Hamline University, Luther College, Knox College, Earthdance Creative Living, Moving Arts Lab, Sierra Contact Festival, wcciJam, Dance New England, Zenon Dance School, the Peaceable Barn, and the Lion's Jaw Festival among others. Will also works in healing justice and as an advocate for artists especially serving as a liaison for artists in experimental forms, people of color and the LGBTQ communities. 
+
+    </p>
+  <!-- <parallax :speed-factor="1">
+  <b-img center src="/static/1bygalen.jpg" fluid/>
+  </parallax> -->
+  </b-col>
+  <!-- <b-col class="p-3">
+  <parallax :speed-factor="1">
+  <b-img center src="/static/ciwbrian.jpg" fluid/>
+  </parallax>
+  </b-col> -->
+  <b-col lg="6" class="p-3">
+  <parallax :speed-factor="1">
+  <b-img center src="/static/ciwbrian.jpg" fluid/>
+  </parallax>
+  </b-col>
+  <b-col class="p-3">
+    <h4>Biography</h4>
+    <p class="text-justify">
+      Taja Will is a queer, Chilean adoptee, performer, choreographer, therapist and restorative justice facilitator. Her approach integrates improvisation, somatic modalities, text and vocals in contemporary  performance. Will’s aesthetic is one of spontaneity, bold choice making, sonic and kinetic partnership and the ability to move in relationship to risk and intimacy.  She explores visceral connections to current socio-cultural realities through feminine archetypes and everyday magic.
+    </p>
+    <p class="text-justify">
+      My work centers relationships. I crave an aesthetic of closeness; which allows performers to move with intimacy and risk, with each other 
+      and in relationship to an audience. When we come into a moment of connection, we do it with the intention to completely engage each other, 
+      without representing intimacy, we commit to being intimate. This is part of the magic, using intention, improvisation and design to create 
+      new works that comment on how I perceive contemporary humanity.
+    </p>
+  </b-col>
+  <b-col class="p-3">hi</b-col>
+
+  </b-row>
+  <b-row>
+      <p>hi</p>
+    </b-row>
+  </div>
+</template>
+
+
+<script>
+  import Parallax from 'vue-parallaxy'
+  export default {
+    components: {
+      Parallax
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  position: relative;
+}
+</style>
